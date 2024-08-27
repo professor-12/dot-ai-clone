07128a5d0a2b0d8a11aea7dc6114510045d88a3c
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-08-27 01:27:25 +0100

    Create the ui-design for stats component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,8 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily: {
-				"inter": ""
+			colors: {
+				"muted": "#868c98"
 			}
 		},
 	},
